acdf6c59fd0d530b8b33934c55f466c4add824fb
Tayrone <tayronetnm@gmail.com>
2020-09-10 07:21:14 -0300

    Tratativa de erro

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,58 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+import api from '../../services/api';
+
+interface Repository {
+	//só as informações necessárias
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	}
+}
+
 const Dashboard: React.FC = () => {
+	const [newRepo, setNewRepo] = useState('');
+	const [inputError, setInputError] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function handleAddRepo(event: FormEvent<HTMLFormElement>): Promise<void> {
+		event.preventDefault();
+		if (!newRepo) {
+			setInputError('Digite o utor/nome do repositório');
+		}
+		
+		try {
+			const response = await api.get<Repository>(`repos/${newRepo}`);
+
+			const repository = response.data;
+	
+			setRepositories([...repositories, repository]);
+			setNewRepo('');
+		} catch (err) {
+			setInputError('Erro ao buscar repositório');
+		}
+	}
+
 	return (
 		<>
 			<img src={logoImg} alt="Github Explorer" />
 			<Title>Explore repositórios no Github</Title>
 
-			<Form>
-				<input placeholder="Digite o nome do repositório"/>
+			<Form onSubmit={handleAddRepo}>
+				<input 
+				value={newRepo}
+				onChange={e => setNewRepo(e.target.value)}
+				placeholder="Digite o nome do repositório"/>
 				<button type="submit">Pesquisar</button>
 			</Form>
 
 			<Repositories>
-				<a href="">
-					<img src="https://avatarfiles.alphacoders.com/249/249080.jpg" alt="" />
+			{repositories.map(repository => (
+					<a key={repository.full_name} href="teste">
+					<img src={repository.owner.avatar_url} alt={repository.owner.login} />
 					<div>
-						<strong>rockeatseat/unforms</strong>
-						<p>Easy peasy highly React Forms! 😍</p>
+						<strong>{repository.full_name}</strong>
+						<p>{repository.description}</p>
 					</div>
 
 					<FiChevronRight size={20} />
 				</a>
-				<a href="">
-					<img src="https://avatarfiles.alphacoders.com/249/249080.jpg" alt="" />
-					<div>
-						<strong>rockeatseat/unforms</strong>
-						<p>Easy peasy highly React Forms! 😍</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
-				<a href="">
-					<img src="https://avatarfiles.alphacoders.com/249/249080.jpg" alt="" />
-					<div>
-						<strong>rockeatseat/unforms</strong>
-						<p>Easy peasy highly React Forms! 😍</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
-				<a href="">
-					<img src="https://avatarfiles.alphacoders.com/249/249080.jpg" alt="" />
-					<div>
-						<strong>rockeatseat/unforms</strong>
-						<p>Easy peasy highly React Forms! 😍</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
+			))}
 			</Repositories>
 		</>
 	)
