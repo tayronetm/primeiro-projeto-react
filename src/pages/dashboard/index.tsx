4f60aa7da74316eee0ca56f86b1003daf0571d49
Tayrone <tayronetnm@gmail.com>
2020-09-10 06:23:55 -0300

    Criação do formuláriio, utilização do polished para estilização

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title } from './styles';
+import { Title, Form } from './styles';
 
 const Dashboard: React.FC = () => {
 	return (
 		<>
 			<img src={logoImg} alt="Github Explorer" />
 			<Title>Explore repositórios no Github</Title>
+
+			<Form>
+				<input placeholder="Digite o nome do repositório"/>
+				<button type="submit">Pesquisar</button>
+			</Form>
 		</>
 	)
 }
--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {shade} from 'polished';
 
 export const Title = styled.h1`
 	font-size: 48px;
@@ -7,4 +8,39 @@ export const Title = styled.h1`
 	line-height: 56px;
 
 	margin-top: 30px;
+`;
+
+export const Form = styled.form`
+	margin-top: 40px;
+	max-width: 700px;
+
+	display: flex;
+
+	input {
+		flex: 1;
+		height: 70px;
+		padding: 0 24px;
+		border-radius: 5px 0 0 5px;
+		color: #3a3a3a;
+
+		&::placeholder {
+			color: #a8a8b3
+		}
+	}
+
+	button {
+		width: 200px;
+		height: 70px;
+		background: #04d361;
+		border-radius: 0px 5px 5px 0px;
+		border: 0;
+		color: #FFF;
+		font-weight: bold;
+		transition: background-color 0.2s;
+
+		/* hover no button */
+		&:hover {
+			background: ${shade(0.2, '#04d361')};
+		}
+	}
 `;
