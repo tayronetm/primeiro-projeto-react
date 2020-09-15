5cfd62a05fb9ebd2808700fc4de65e9a77bd2878
Monteiro <tayrone.n.monteiro@accenture.com>
2020-09-15 07:48:16 -0300

    Local Storage, API e Rotas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -20,7 +21,22 @@ interface Repository {
 const Dashboard: React.FC = () => {
 	const [newRepo, setNewRepo] = useState('');
 	const [inputError, setInputError] = useState('');
-	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	const [repositories, setRepositories] = useState<Repository[]>(() => {
+		const storagedRepositories = localStorage.getItem(
+			'@GithubExplorer:repositories',
+		);
+		if (storagedRepositories) {
+			return JSON.parse(storagedRepositories);
+		}
+		return [];
+	});
+
+	useEffect(() => {
+		localStorage.setItem(
+			'@GithubExplorer: repositories', 
+			JSON.stringify(repositories))
+	}, [repositories])
 
 	async function handleAddRepo(event: FormEvent<HTMLFormElement>): Promise<void> {
 		event.preventDefault();
@@ -60,7 +76,7 @@ const Dashboard: React.FC = () => {
 
 			<Repositories>
 			{repositories.map(repository => (
-					<a key={repository.full_name} href="teste">
+				<Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
 					<img src={repository.owner.avatar_url} alt={repository.owner.login} />
 					<div>
 						<strong>{repository.full_name}</strong>
@@ -68,7 +84,7 @@ const Dashboard: React.FC = () => {
 					</div>
 
 					<FiChevronRight size={20} />
-				</a>
+				</Link>
 			))}
 			</Repositories>
 		</>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ import Repository from '../pages/repository';
 const Routes: React.FC = () => (
 	<Switch>
 		<Route path="/" exact component={Dashboard} />
-		<Route path="/" component={Repository} />
+		<Route path="/repositories/:repository+" component={Repository} />
 	</Switch>
 );
 
